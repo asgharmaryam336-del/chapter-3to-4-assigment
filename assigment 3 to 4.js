cb167d22e983532f6828c4963ad1049324b09f0c
Maryam Asghar <asgharmaryam336@gmail.com>
2026-05-30 09:42:54 +0500

    Add files via upload

---
+++ assigment 3 to 4.js
@@ -0,0 +1,42 @@
+
+//chapter 3
+//problem 1
+let age = 10;
+alert("I am " + age + " years old ");
+
+//problem 2
+let visitcount = 14;
+alert("You have visited this site " + visitcount + " times ");
+
+//problem 3
+let visitorName = "ayesha";
+let productTitle = "T-shirt(s)";
+let quantity = 5;
+
+document.write("<b> " + visitorName + "</b> ordered <b>" + quantity + ' ' + productTitle + "</b> on XYZ Clothing store");
+
+//chapter 4
+//problem 1
+let name = "ali", userAge = 20, course = "web development";
+
+
+//problem 2
+let userName = "ayesha";
+let _total = 500;
+let $price = 99;
+let my1stVariable = "test";
+let camelCase = true;
+
+//problem 3
+document.write("<h1> Rules for naming JS variable </h1>");
+document.write("b)<b> Variable names can only contain <b> letter</b> , <b> numbers </b>,  <b>$</b>,  <b> _</b> .For example  <b> $my_1stVariable</b>  <br><br>");
+document.write("c) Variable must begins with a <b> letter</b> , <b> $ </b>,  <b>_</b>. For example  <b> $_name</b> or <b>name</br> <br><br>");
+document.write("d) Variable names are case <b>sensitive</b><br><br>");
+document.write("e) Variable names should not be JS <b> keywords</b>");
+
+
+
+
+
+
+
